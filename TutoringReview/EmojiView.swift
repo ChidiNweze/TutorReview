5cfb4c840abb9750b93e2bf639e426be79a6d2b2
Chidinma Umenwofor-Nweze <c.umenwofornweze@gmail.com>
2020-05-09 16:23:28 -0400

    Add EmojiView.swift

---
+++ TutoringReview/EmojiView.swift
@@ -0,0 +1,21 @@
+//
+//  EmojiView.swift
+//  TutoringReview
+//
+//  Created by Chidinma Umenwofor-Nweze on 2020-05-09.
+//  Copyright © 2020 Chidinma Umenwofor-Nweze. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmojiView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiView()
+    }
+}
