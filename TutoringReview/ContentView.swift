ada0fb11643ef8bffd847b06a75fd70864813098
Chidinma Umenwofor-Nweze <c.umenwofornweze@gmail.com>
2020-05-08 21:20:24 -0400

    Initial Commit

---
+++ TutoringReview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TutoringReview
+//
+//  Created by Chidinma Umenwofor-Nweze on 2020-05-08.
+//  Copyright © 2020 Chidinma Umenwofor-Nweze. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
