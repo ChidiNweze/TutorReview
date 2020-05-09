0873e7a3e164f39468b8091f22a9ae85b08aa69d
Chidinma Umenwofor-Nweze <c.umenwofornweze@gmail.com>
2020-05-08 21:53:51 -0400

    Create review struct. Hard code example reviews. Arrange them in list view.

--- TutoringReview/ContentView.swift
+++ TutoringReview/ContentView.swift
@@ -8,9 +8,25 @@
 
 import SwiftUI
 
+struct Review {
+    var student: String
+    var comment: String
+    var rating: String
+}
+
 struct ContentView: View {
+    
+    let reviews = [
+        Review(student: "John Doe", comment: "Moved through content a little too fast, but then was very helpful and clear when it came to my specific questions.", rating: "Thumbs Neutral"), Review(student: "Jane Doe", comment: "Loved all the study tips! Jumbling up the order of math exercises was daunting a first but paid off when the test came around.", rating: "Thumbs Up"), Review(student: "George Washington", comment: "Explanation style was a bit unprofessional in my opinion.", rating: "Thumbs Down")
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        List {
+            /*each review needs a unique identifier. Instead of making the Review struct conform to the identifiable protocol, I decided that each student can only review me once. Therefore, each student is a unique identifier*/
+            ForEach(reviews, id: \.student) { review in
+                Text(review.student)
+            }
+        }
     }
 }
 
