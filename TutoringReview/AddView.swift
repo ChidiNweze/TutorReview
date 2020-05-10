e3a01cb54e0230e0f091bb75137849ff358873d6
Chidinma Umenwofor-Nweze <c.umenwofornweze@gmail.com>
2020-05-10 18:31:37 -0400

    Add AddView.swift

---
+++ TutoringReview/AddView.swift
@@ -0,0 +1,21 @@
+//
+//  AddView.swift
+//  TutoringReview
+//
+//  Created by Chidinma Umenwofor-Nweze on 2020-05-10.
+//  Copyright © 2020 Chidinma Umenwofor-Nweze. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
