8f93e1a84a3f7d9285476a523d99e473c12ba478
Chidinma Umenwofor-Nweze <c.umenwofornweze@gmail.com>
2020-05-11 22:35:07 -0400

    Create basic  UI for AddView.swift

--- TutoringReview/AddView.swift
+++ TutoringReview/AddView.swift
@@ -9,8 +9,42 @@
 import SwiftUI
 
 struct AddView: View {
+    @Environment(\.managedObjectContext) var moc
+    
+    @State var student = ""
+    @State var comment = ""
+    @State var rating = "Silence"
+    let ratings = ["Thumbs Up", "Neutral", "Thumbs Down", "Silence"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section {
+                    TextField("Student Name", text: $student)
+                    TextField("Feedback", text: $comment)
+                    
+                    Picker("Rating", selection: $rating) {
+                        ForEach(ratings, id: \.self) { rating in
+                            Text(rating)
+                        }
+                    }
+                }
+                
+                Button("Add Review") {
+                    //adding new review to database
+                    let newReview = Review(context: self.moc)
+                    newReview.student = self.student
+                    newReview.comment = self.comment
+                    newReview.rating = self.rating
+                    
+                    do {
+                        try self.moc.save()
+                    } catch {
+                        print("Could not save new review to database. \(error.localizedDescription)")
+                    }
+                }
+            }.navigationBarTitle("Review Tutor")
+        }
     }
 }
 
